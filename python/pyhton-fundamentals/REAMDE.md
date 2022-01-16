55febb5e3953feeee194e6ac8fa8a21fd0f3e51d
Pablo <botterogandia@gmail.com>
2022-01-16 15:58:59 +0100

    Basic Python

---
+++ python/pyhton-fundamentals/REAMDE.md
@@ -0,0 +1,5 @@
+## Python Fundamentals
+
+In this folder you will find a series of exercises that [Martín San José](https://replit.com/@masajo) gave us to review everything we have learned about Python. 
+
+In the file main.py are all the exercises listed, you only have to uncomment them to execute them.
---
+++ python/pyhton-fundamentals/main.py
@@ -0,0 +1,199 @@
+'''
+Basic tasks
+'''
+
+# task 1 - Basics
+# from basics.task1 import task1Basic
+# task1Basic()
+
+# task 2 - Basics
+# from basics.task2 import task2Basic
+# task2Basic()
+
+# task 3 - Basics
+# from basics.task3 import task3Basic
+# task3Basic()
+
+# task 4 - Basics
+# from basics.task4 import task4Basic
+# task4Basic()
+
+# task 5 - Basics
+# from basics.task5 import task5Basic
+# task5Basic()
+
+# task 6 - Basics
+# from basics.task6 import task6Basic
+# task6Basic()
+
+# task 7 - Basics
+# from basics.task7 import task7Basic
+# task7Basic()
+
+# task 8 - Basics
+# from basics.task8 import task8Basic
+# task8Basic()
+
+# task 9 - Basics
+# from basics.task9 import task9Basic
+# task9Basic()
+
+# task 10 - Basics
+# from basics.task10 import task10Basic
+# task10Basic()
+
+# task 11 - Basics
+# from basics.task11 import task11Basic
+# task11Basic()
+
+# task 12 - Basics
+# from basics.task12 import task12Basic
+# task12Basic()
+
+# task 13 - Basics
+# from basics.task13 import task13Basic
+# task13Basic()
+
+# task 14 - Basics
+# from basics.task14 import task14Basic
+# task14Basic()
+
+# task 15 - Basics
+# from basics.task15 import task15Basic
+# task15Basic()
+
+# task 16 - Basics
+# from basics.task16 import task16Basic
+# task16Basic()
+
+# task 17 - Basics
+# from basics.task17 import task17Basic
+# task17Basic()
+
+# task 18 - Basics
+# from basics.task18 import task18Basic
+# task18Basic()
+
+'''
+Advanced tasks
+'''
+
+# task 1
+# from advanced.task1 import task1advanced
+# task1advanced()
+
+# task 2
+# from advanced.task2 import task2advanced
+# task2advanced()
+
+# task 3
+# from advanced.task3 import task3advanced
+# task3advanced()
+
+# task 4
+# from advanced.task4 import task4advanced
+# task4advanced()
+
+# task 5
+# from advanced.task5 import task5advanced
+# task5advanced()
+
+# task 6
+# from advanced.task6 import task6advanced
+# task6advanced()
+
+# task 7
+# from advanced.task7 import task7advanced
+# task7advanced()
+
+# task 8
+# from advanced.task8 import task8advanced
+# task8advanced()
+
+# task 9
+# from advanced.task9 import task9advanced
+# task9advanced()
+
+# task 10
+# from advanced.task10 import task10advanced
+# task10advanced()
+
+# task 11
+# from advanced.task11 import task11advanced
+# task11advanced()
+
+# task 12
+# from advanced.task15 import task12advanced
+# task12advanced()
+
+# task 13
+# from advanced.task13 import task13advanced
+# task13advanced()
+
+# task 14
+# from advanced.task14 import task14advanced
+# task14advanced()
+
+# task 15
+# from advanced.task15 import task15advanced
+# task15advanced()
+
+# task 16
+# from advanced.task16 import task16advanced
+# task16advanced()
+
+# task 17
+# from advanced.task17 import task17advanced
+# task17advanced()
+
+# task 18
+# from advanced.task18 import task18advanced
+# task18advanced()
+
+# task 19
+# from advanced.task19 import task19advanced
+# task19advanced()
+
+# task 20
+# from advanced.task20 import task20advanced
+# task20advanced()
+
+# task 21
+# from advanced.task21 import task21advanced
+# task21advanced()
+
+# task 22
+# from advanced.task22 import task22advanced
+# task22advanced()
+
+# task 23
+# from advanced.task23 import task23advanced
+# task23advanced()
+
+# task 24
+# from advanced.task24 import task24advanced
+# task24advanced()
+
+# task 25
+# from advanced.task25 import task25advanced
+# task25advanced()
+
+# task 26
+# from advanced.task26 import task26advanced
+# task26advanced()
+
+# task 27
+# from advanced.task27 import task27advanced
+# task27advanced()
+
+# task 28
+# from advanced.task28 import task28advanced
+# task28advanced()
+
+# task 29
+# from advanced.task29 import task29advanced
+# task29advanced()
+
+# task 30
+# from advanced.task30 import task30advanced
+# task30advanced()
